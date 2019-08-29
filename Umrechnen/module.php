2c13e816a50faad6483f3375d1ae8706d45d7795
Till Brede <tb@symcon.de>
2019-08-29 11:55:08 +0200

    applied cs fixer style

--- Umrechnen/module.php
+++ Umrechnen/module.php
@@ -1,50 +1,45 @@
-<?
+<?php
 
-	class Umrechnen extends IPSModule
-	{
-		
-		public function Create()
-		{
-			//Never delete this line!
-			parent::Create();
-			
-			$this->RegisterPropertyInteger("SourceVariable", 0);
-			$this->RegisterPropertyString("Formula", "\$Value/10*sin(30)*pi()");
-			
-			$this->RegisterVariableFloat("Value", "Value", "", 0);
-		}
-	
-		public function ApplyChanges()
-		{
-			
-			//Never delete this line!
-			parent::ApplyChanges();
+declare(strict_types=1);
 
-			$eid = @$this->GetIDForIdent("SourceTrigger");
-			if($eid) {
-				IPS_DeleteEvent($eid);
-			}
+    class Umrechnen extends IPSModule
+    {
+        public function Create()
+        {
+            //Never delete this line!
+            parent::Create();
 
-			if(IPS_VariableExists($this->ReadPropertyInteger("SourceVariable"))) {
-				$this->RegisterMessage(($this->ReadPropertyInteger("SourceVariable")), VM_UPDATE);
-			}
-			
-		}
-	
-		public function MessageSink ($TimeStamp, $SenderID, $Message, $Data)
-		{
-			SetValue($this->GetIDForIdent("Value"), $this->Calculate($Data[0]));
-		}
+            $this->RegisterPropertyInteger('SourceVariable', 0);
+            $this->RegisterPropertyString('Formula', '$Value/10*sin(30)*pi()');
 
-		public function Calculate(float $Value)
-		{
-			
-			eval("\$Value = " . $this->ReadPropertyString("Formula") . ";");
-			
-			return $Value;
-		
-		}
-	
-	}
+            $this->RegisterVariableFloat('Value', 'Value', '', 0);
+        }
 
-?>
+        public function ApplyChanges()
+        {
+
+            //Never delete this line!
+            parent::ApplyChanges();
+
+            $eid = @$this->GetIDForIdent('SourceTrigger');
+            if ($eid) {
+                IPS_DeleteEvent($eid);
+            }
+
+            if (IPS_VariableExists($this->ReadPropertyInteger('SourceVariable'))) {
+                $this->RegisterMessage(($this->ReadPropertyInteger('SourceVariable')), VM_UPDATE);
+            }
+        }
+
+        public function MessageSink($TimeStamp, $SenderID, $Message, $Data)
+        {
+            SetValue($this->GetIDForIdent('Value'), $this->Calculate($Data[0]));
+        }
+
+        public function Calculate(float $Value)
+        {
+            eval('$Value = ' . $this->ReadPropertyString('Formula') . ';');
+
+            return $Value;
+        }
+    }
